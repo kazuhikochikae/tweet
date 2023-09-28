e2922d46866b526e2041eefe7732c065f2396451
“kazuhikochikae” <kazuhikochikae@gmail.com>
2023-09-28 16:14:40 +0900

    Strong Parameters

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,12 +2,18 @@ class BlogsController < ApplicationController
   def index
   end
 
-   def new
+  def new
     @blog = Blog.new
-   end
+  end
 
-   def create
-    Blog.create(content: params[:blog][:content])
+  def create
+    Blog.create(blog_params)
     redirect_to new_blog_path
-   end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
 end
