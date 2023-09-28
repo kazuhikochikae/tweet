0f60b5f11c01d23bc73be638a29fdcd649f35812
“kazuhikochikae” <kazuhikochikae@gmail.com>
2023-09-28 15:58:33 +0900

    “リダイレクト処理“

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,5 +3,11 @@ class BlogsController < ApplicationController
   end
 
    def new
+    @blog = Blog.new
+   end
+
+   def create
+    Blog.create(content: params[:blog][:content])
+    redirect_to new_blog_path
    end
 end
